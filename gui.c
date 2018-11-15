d901094220b049394be62f3a8c174507931bd671
Ben Moyer <ben.s.moyer@gmail.com>
2018-11-15 20:24:25 +0000

    gui working with improved code

--- gui.c
+++ gui.c
@@ -1,7 +1,25 @@
 #include <gtk/gtk.h>
 
+#include <X11/Xos.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <X11/Xresource.h>
+
+#include "color_detect.h"
+
+#define MAX_COLORS   1000
+
 /* Surface to store current scribbles */
+static Window root;
 static cairo_surface_t *surface = NULL;
+Display* d;
+GThread* thread;
+GtkWidget* color_name_label;
+GtkWidget* rgb_label;
+GMainContext *context;
+
+color* colors;
 
 static void
 clear_surface (void)
@@ -129,6 +147,110 @@ close_window (void)
     cairo_surface_destroy (surface);
 }
 
+    static void
+query_pointer(int* x, int* y)
+{
+    static int once;
+    int i;
+    unsigned m;
+    Window w;
+
+    if (once == 0) {
+        once = 1;
+        root = DefaultRootWindow(d);
+    }
+
+    if (!XQueryPointer(d, root, &root, &w, x, y, &i, &i, &m)) {
+        for (i = -1; ++i < ScreenCount(d); ) {
+            if (root == RootWindow(d, i)) {
+                break;
+            } 
+        }
+    }
+}
+
+void get_color(Display* d, int x, int y, int* r, int* b, int* g) {
+    XColor c;
+    XImage *image = XGetImage (d, XRootWindow (d, XDefaultScreen (d)), x, y, 1, 1, AllPlanes, XYPixmap);
+    c.pixel = XGetPixel (image, 0, 0);
+    XFree (image);
+    XQueryColor (d, XDefaultColormap(d, XDefaultScreen (d)), &c);
+
+    *r = (c.red/256);
+    *b = (c.blue/256);
+    *g = (c.green/256);
+    //printf("RGB was: (%d,%d,%d)", *r,*g,*b);
+}
+
+    static gboolean
+update_color(gpointer user_data)
+{
+    int r, g, b, x, y;
+    query_pointer(&x, &y);
+    char s2[20];
+    //get_color(d, x, y, &r, &g, &b);
+    get_color(d, x, y, &r, &b, &g);
+    
+    // set RGB readout
+    sprintf(s2, "%d, %d, %d", r, g, b);
+    gtk_label_set_label(GTK_LABEL(rgb_label),s2);
+
+    // set name readout
+    int yc, uc, vc;
+    yuv_from_rgb(&yc, &uc, &vc, r, g, b);
+    //color c = nearest_color(yc, uc, vc, colors, MAX_COLORS);
+    color c = nearest_color(r,g,b, colors, MAX_COLORS);
+    gtk_label_set_label(GTK_LABEL(color_name_label), c.name);
+
+    return G_SOURCE_REMOVE;
+}
+
+static gpointer thread_func(gpointer user_data)
+{
+    //int n_thread = GPOINTER_TO_INT(user_data);
+    //Display* d = (Display*)user_data;
+    GSource *source;
+
+    //g_print("Starting thread %d\n", n_thread);
+
+    for (;;) {
+        g_usleep(5000);
+        //query_pointer(d);
+        source = g_idle_source_new();
+        g_source_set_callback(source, update_color, NULL, NULL);
+        g_source_attach(source, context);
+        g_source_unref(source);
+    }
+
+    //g_print("Ending thread %d\n", n_thread);
+    return NULL;
+}
+
+
+static int catchFalseAlarm(void)
+{
+    return 0;
+}   
+
+void setup_x11()
+{
+  XSetWindowAttributes attribs;
+  Window w;
+
+  if (!(d = XOpenDisplay(0))) {
+      fprintf (stderr, "Couldn't connect to %s\n", XDisplayName (0));
+      exit (EXIT_FAILURE);
+  }
+
+  attribs.override_redirect = True;
+  w = XCreateWindow(d, DefaultRootWindow (d), 800, 800, 1, 1, 0,
+          CopyFromParent, InputOnly, CopyFromParent,
+          CWOverrideRedirect, &attribs);
+  XMapWindow(d, w);
+  XSetErrorHandler((XErrorHandler )catchFalseAlarm);
+  XSync (d, 0);
+}
+
 static void
 activate (GtkApplication *app,
           gpointer        user_data)
@@ -137,8 +259,8 @@ activate (GtkApplication *app,
   GtkWidget *frame;
   GtkWidget *drawing_area;
   GtkWidget* box;
-  GtkWidget* color_name_label;
-  GtkWidget* rgb_label;
+
+  setup_x11();
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "color helper");
@@ -187,6 +309,9 @@ activate (GtkApplication *app,
                                      | GDK_BUTTON_PRESS_MASK
                                      | GDK_POINTER_MOTION_MASK);
 
+  // start update thread
+  thread = g_thread_new(NULL, thread_func, (gpointer)d);
+
   gtk_widget_show_all (window);
 }
 
@@ -194,12 +319,14 @@ int
 main (int    argc,
       char **argv)
 {
+  colors = read_colors();
   GtkApplication *app;
   int status;
 
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_thread_join(thread);
   g_object_unref (app);
 
   return status;
